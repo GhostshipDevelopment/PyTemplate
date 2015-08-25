3c1e213fd221aaa6b8be4941534fbf3c7dc800bc
ThatITNinja <ThatITNinja@hotmail.com>
2015-08-25 14:16:29 -0400

    more work with example

--- pytemplate/template.py
+++ pytemplate/template.py
@@ -6,20 +6,33 @@ class DataTypeReader(object):
         self.endian = endian
     def integer(self, f_obj):
         return self._dtype_gen_read("i", 1, 4, f_obj)
+    def short(self, f_obj):
+        return self._dtype_gen_read("h", 1, 2, f_obj)
     def string(self, f_obj, st_length):
         return self._dtype_gen_read("s", st_length, st_length, f_obj)
     def char(self, f_obj):
         return self._dtype_gen_read("c", 1, 1, f_obj)
+    def byte(self, f_obj):
+        return self._dtype_gen_read("b", 1, 1, f_obj)
     def _dtype_gen_read(self, dtype, dtype_count, dtype_size, f_obj):
-        yield unpack("%s%s%s" % (self.endian, dtype_count, dtype), f_obj.read(dtype_size))
+        if dtype_count > 1:
+            yield unpack("%s%s%s" % (self.endian, dtype_count, dtype), f_obj.read(dtype_size))
+        else:
+            yield unpack("%s%s%s" % (self.endian, dtype_count, dtype), f_obj.read(dtype_size))[0]
 
 class DataTypeWriter(object):
     def __init__(self, endian="<"):
         self.endian = endian
     def integer(self, f_obj, value):
         return self._dtype_gen_write("i", value, 1, f_obj)
+    def short(self, f_obj, value):
+        return self._dtype_gen_write("h", value, 1, f_obj)
     def string(self, f_obj, value):
         return self._dtype_gen_write("s", value, len(value), f_obj)
+    def char(self, f_obj, value):
+        return self._dtype_gen_write("c", value, 1, f_obj)
+    def byte(self, f_obj, value):
+        return self._dtype_gen_write("b", value, 1, f_obj)
     def _dtype_gen_write(self, dtype, value, dtype_count, f_obj):
         yield f_obj.write(pack("%s%s%s" % (self.endian, dtype_count, dtype), value))
 
@@ -29,9 +42,9 @@ class Template(object):
     def define(self, odict):
         self.definition = odict
     def extract(self):
-        values = {}
-        for v_name, v_gen_read in self.definition.iteritems():
-            values[v_name] = list(v_gen_read)
+        values = dict()
+        for v_name in self.definition:
+            values[v_name] = list(self.definition[v_name])[0]
         return values
     def write(self):
         for v_name, v_gen_write in self.definition.iteritems():
---
+++ test/examples/sqlite3_driver.py
@@ -0,0 +1,66 @@
+from collections import OrderedDict as OD
+from pytemplate import DataTypeReader as DTR_U
+from pytemplate import DataTypeWriter as DTW_U
+from pytemplate import Template as T
+
+#create bindings for read/write objects
+DTR = DTR_U()
+DTW = DTW_U()
+
+#for testing
+import sqlite3
+import os
+
+#easy dbg
+from pprint import PrettyPrinter as PP
+p = PP().pprint
+
+def read_header(f_obj):
+    template = T()
+    t_dict = OD()
+    t_dict["header_string"] = DTR.string(f_obj, 16)
+    t_dict["page_size"] = DTR.short(f_obj)
+    t_dict["ff_write_version"] = DTR.byte(f_obj)
+    t_dict["ff_read_version"] = DTR.byte(f_obj)
+    t_dict["max_embedd_payload_frac"] = DTR.byte(f_obj)
+    t_dict["min_embedd_payload_frac"] = DTR.byte(f_obj)
+    t_dict["leaf_payload_frac"] = DTR.byte(f_obj)
+    t_dict["f_change_counter"] = DTR.integer(f_obj)
+    t_dict["header_db_size"] = DTR.integer(f_obj)
+    t_dict["first_freelist_trunk_page_num"] = DTR.integer(f_obj)
+    t_dict["freelist_trunk_page_count"] = DTR.integer(f_obj)
+    t_dict["schema_cookie_offset"] = DTR.integer(f_obj)
+    t_dict["schema_format_number"] = DTR.integer(f_obj)
+    t_dict["default_page_cache_size"] = DTR.integer(f_obj)
+    t_dict["largest_root_btree_page"] = DTR.integer(f_obj)
+    t_dict["db_text_encoding"] = DTR.integer(f_obj)
+    t_dict["user_version"] = DTR.integer(f_obj)
+    t_dict["vacuum_mode"] = DTR.integer(f_obj)
+    t_dict["application_id"] = DTR.integer(f_obj)
+    t_dict["expansion_space"] = DTR.string(f_obj, 20)
+    t_dict["version_valid_for"] = DTR.integer(f_obj)
+    t_dict["sqlite_version_number"] = DTR.integer(f_obj)
+    template.define(
+        t_dict
+    )
+    return template.extract()
+
+def _main_create_db(path):
+    connection = sqlite3.connect(path)
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE test(var1 int, var2 int);")
+    connection.commit()
+    connection.close()
+
+def main():
+    _main_create_db("test.db")
+    with open("test.db", "rb") as db_file:
+        header = read_header(db_file)
+        print db_file.tell()
+        p(header)
+    if os.path.exists("test.db"):
+        os.remove("test.db")
+
+
+if __name__ == '__main__':
+    main()
