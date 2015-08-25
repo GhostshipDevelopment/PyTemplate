f3b6ff8b96c65b1e7b70783e51e3e49dbd796921
ThatITNinja <ThatITNinja@hotmail.com>
2015-08-25 11:45:01 -0400

    remove testing function

--- core/template.py
+++ core/template.py
@@ -36,33 +36,3 @@ class Template(object):
     def write(self):
         for v_name, v_gen_write in self.definition.iteritems():
             list(v_gen_write)
-
-
-def test():
-    my_file = open("test.txt", "wb")
-    dt = DataTypeWriter()
-    template_writer = Template()
-    template_writer.define(
-        OD(
-            my_var = dt.integer(my_file, 10),
-            my_string = dt.string(my_file, "testing")
-        )
-    )
-    template_writer.write()
-    my_file.close()
-
-    my_file_2 = open("test.txt", "rb")
-    dt2 = DataTypeReader()
-    template_reader = Template()
-    template_reader.define(
-        OD(
-            my_var = dt2.integer(my_file_2),
-            my_string = dt2.string(my_file_2, len("testing"))
-        )
-    )
-    t_values = template_reader.extract()
-    my_file_2.close()
-    print t_values
-
-if __name__ == '__main__':
-    test()
